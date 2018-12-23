330710c826ccd70ca11b937d1065275c19c6f08e
agarcian031 <algarcian031@outlook.com>
2018-12-23 00:03:15 -0600

    Add player input and functionality

--- app.js
+++ app.js
@@ -10,7 +10,23 @@ function computerPlayer() {
     return gameChoice; 
 }
 
-// PLAYER
+// PLAYER - prompt player for input 
 function opPlayer() {
-    
+    let playerPrompt = prompt("Please choose Rock, Paper, or Scissors:");
+    let playerChoice = playerPrompt.toLowerCase(); 
+
+    // If user enters choice, return user choice 
+    if (playerChoice=="rock" || playerChoice=="paper" || playerChoice=="scissors") {
+        alert(`You have selected ${playerChoice} as your weapon`); 
+        return playerChoice; 
+    }
+    // if user does not enter choice, prompt again
+    else if (playerChoice==null || playerChoice=="") {
+        alert("Oops! You need to enter Rock, Paper, or Scissors!"); 
+        opPlayer()
+    }
+    else {
+        alert ("Please choose Rock, Paper, or Scissors:"); 
+        opPlayer()
+    }
 }
