8bd16f74224814c4a31aab91fe351668deb519bb
agarcian031 <algarcian031@outlook.com>
2018-12-23 00:25:50 -0600

    Added player/computer scenarios

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ function computerPlayer() {
     let gameOptions = ["Rock", "Paper", "Scissors"]; 
     let gameChoice = gameOptions[Math.floor(Math.random() * gameOptions.length)].toLowerCase(); 
     return gameChoice; 
-}
+}// end function computerPLayer 
 
 // PLAYER - prompt player for input 
 function opPlayer() {
@@ -28,5 +28,65 @@ function opPlayer() {
     else {
         alert ("Please choose Rock, Paper, or Scissors:"); 
         opPlayer()
-    }
-}
+    } //end else 
+} //end function opPlayer 
+
+// GAME FUNCTIONS 
+function game() {
+
+    while(counter<5) {
+        function executeRound(playerSelection, computerSelection){
+            if(playerSelection=="rock" && computerSelection=="scissors"){
+                playerScore++;
+                counter+=1; 
+                alert("You win! Rock crushes Scissors!"); 
+            }
+            else if(playerSelection=="paper" && computerSelection=="rock"){
+                playerScore++; 
+                counter+=1; 
+                alert("You win! Paper covers Rock!"); 
+            }
+            else if(playerSelection=="scissors" && computerSelection=="paper"){
+                playerScore++; 
+                counter+=1; 
+                alert("You win! Scissors cuts Paper!"); 
+
+            }
+            else if(playerSelection==computerSelection){
+                counter+=1; 
+                alert("Ooh! It's a tie!"); 
+            }
+            else {
+                counter+=1; 
+                computerScore++; 
+                alert(`Sorry! Computer wins! \n ${computerSelection} trumps ${playerSelection}`); 
+            }
+        }//end function executeRound 
+
+        let playerSelection = opPlayer(); 
+        let computerSelection = computerPlayer();
+        executeRound(playerSelection, computerSelection); 
+        
+        
+        if(counter<5){
+            if(playerScore>computerScore){
+                alert(`You are currently winning! \n Score Break Down: You: ${playerScore} Computer: ${computerScore}`);
+            }
+            else if(computerScore > playerScore){
+                alert(`Computer is currently winning! \n Score Break Down: You: ${playerScore} Computer: ${computerScore}`);
+            }
+            else if(playerScore == computerScore){
+                alert(`Both players are currently tied! \n Score Break Down: You: ${playerScore} Computer: ${computerScore}`); 
+            }
+        }
+        else if(counter==5){
+            if(playerScore>computerScore){
+                alert(`You win! \n Your Score: ${playerScore} \n Computer Score: ${computerScore}`);
+            }
+            else if(computerScore > playerScore){
+                alert(`Computer wins! \n Your Score: ${playerScore} \n Computer Score: ${computerScore}`);
+
+            }//end else
+        }//end else if
+    } //end while 
+} //end function 
